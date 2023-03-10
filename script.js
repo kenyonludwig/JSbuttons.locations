da755474fd928ba70912a240173a45205e936c80
kenyonludwig <111588304+kenyonludwig@users.noreply.github.com>
2023-03-10 08:22:34 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,35 +1,36 @@
 function aboutMe1() {
     document.getElementById("demo").innerHTML = "Hello, this is the 'About Me Page' where you'll find out more about me!";
-}
-
-function aboutMe2() {
+  }
+  
+  function aboutMe2() {
     document.getElementById("divElement").style.decoration = "underline";
-}
-
-function aboutMe3() {
+  }
+  
+  function aboutMe3() {
     document.getElementById("div123").style.color = "red";
-}
-
-function findMe1() {
+  }
+  
+  function findMe1() {
     document.getElementById("findMe").style.transform = "capitalize";
-}
-
-function findMe2() {
+  }
+  
+  function findMe2() {
     document.getElementById("findMe").style.color = "green";
-}
-
-function findMe3() {
+  }
+  
+  function findMe3() {
     document.getElementById("findMe").style.decoration = "dashed";
-}
-
-function orderGoods1() {
+  }
+  
+  function orderGoods1() {
     document.getElementById("orderGoods").style.border = "5px solid blue";
-}
-
-function orderGoods2() {
+  }
+  
+  function orderGoods2() {
     document.getElementById("dot").style.width = "10px";
-}
-
-function orderGoods3() {
+  }
+  
+  function orderGoods3() {
     document.getElementById("dot").style.color = "orange";
-}
+  }
+  
